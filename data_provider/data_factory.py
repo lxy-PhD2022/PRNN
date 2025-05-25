ddc8dc423be6cce34a24f768df0162a87efdfb6e
lxy <155620433+lxy-PhD2022@users.noreply.github.com>
2025-05-25 10:59:43 +0800

    Update data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,4 +1,4 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Pred
+from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_Solar, Dataset_PEMS, Dataset_Pred
 from torch.utils.data import DataLoader
 
 data_dict = {
@@ -6,6 +6,9 @@ data_dict = {
     'ETTh2': Dataset_ETT_hour,
     'ETTm1': Dataset_ETT_minute,
     'ETTm2': Dataset_ETT_minute,
+    'custom': Dataset_Custom,
+    'Solar': Dataset_Solar,
+    'PEMS': Dataset_PEMS,
     'custom': Dataset_Custom,
 }
 
